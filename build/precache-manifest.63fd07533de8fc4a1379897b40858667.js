929d57a09edc1136a577932f08b6dd29a90cf6db
T K <kytomaa@gmail.com>
2020-04-07 15:19:23 +0300

    added frontend production version

---
+++ build/precache-manifest.63fd07533de8fc4a1379897b40858667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bc6a8b374c1c2ef8e2a878fef701b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eaee768c56a419fcaff",
+    "url": "/static/css/main.4e29f17c.chunk.css"
+  },
+  {
+    "revision": "4c0cc097d386c69b1dcb",
+    "url": "/static/js/2.5333a41c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5333a41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eaee768c56a419fcaff",
+    "url": "/static/js/main.fee68568.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
