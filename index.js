74a4952af5bcf5bd8e6230cccdd576410e40738f
T K <kytomaa@gmail.com>
2020-04-07 13:14:19 +0300

    pushing to Heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 // app.use(morgan('tiny'))
@@ -123,7 +125,7 @@ app.post('/api/persons/', (req, res) => {
 //   res.end(JSON.stringify(persons))
 // })
 
-const port = 3001
+const port = process.env.port || 3001
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
 })
